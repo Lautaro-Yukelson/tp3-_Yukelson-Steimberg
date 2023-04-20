5671113d0f25f541fa48297fba07f94048aa8847
lautiyuke <lautiyuke@gmail.com>
2023-04-20 19:31:13 -0300

    Detalles y funcion EstadisticasEvento() completa

--- Program.cs
+++ Program.cs
@@ -1,109 +1,127 @@
-﻿using System;
+﻿    using System;
 
-menu();
+    bool habilitado = false;
+    Ticketera miTicketera = new Ticketera();
+    menu();
 
 
-void menu(){
-    Console.Clear();
-    Console.WriteLine("*******************************");
-    Console.WriteLine("¡Bienvenido al InfoPalooza 2023");
-    Console.WriteLine("*******************************");
-    Console.WriteLine("Eliga una opción para continuar");
-    Console.WriteLine("1. Nueva Incripcion");
-    Console.WriteLine("2. Obtener Estadisticas del Evento");
-    Console.WriteLine("3. Buscar Cliente");
-    Console.WriteLine("4. Cambiar Entrada de un Cliente");
-    Console.WriteLine("5. Salir");
-    int opcion = int.Parse(Console.ReadLine());
-    while (opcion < 1 || opcion > 5) opcion = ingresarEntero("Esa opcion no existe, ingrese una valida");
-    switch(opcion){
-        case 1:
-            nuevaIncripcion();
-            break;
-        case 2:
-            estadisticasEvento();
-            break;
-        case 3:
-            buscarCliente();
-            break;
-        case 4:
-            cambiarEntrada();
-            break;
-        case 5:
+    void menu(){
+        Console.Clear();
+        Console.WriteLine("*******************************");
+        Console.WriteLine("¡Bienvenido al InfoPalooza 2023");
+        Console.WriteLine("*******************************");
+        Console.WriteLine("Eliga una opción para continuar");
+        Console.WriteLine("1. Nueva Incripcion");
+        Console.WriteLine("2. Obtener Estadisticas del Evento");
+        Console.WriteLine("3. Buscar Cliente");
+        Console.WriteLine("4. Cambiar Entrada de un Cliente");
+        Console.WriteLine("5. Salir");
+        int opcion = int.Parse(Console.ReadLine());
+        while (opcion < 1 || opcion > 5) opcion = ingresarEntero("Esa opcion no existe, ingrese una valida");
+        switch(opcion){
+            case 1:
+                nuevaIncripcion();
+                habilitado = true;
+                menu();
+                break;
+            case 2:
+                if (habilitado) estadisticasEvento();
+                else{
+                    Console.WriteLine("Aun no se anoto nadie");
+                    Console.WriteLine("Presione una telca para continuar...");
+                    Console.ReadKey();
+                }
+                menu();
+                break;
+            case 3:
+                if (habilitado) buscarCliente();
+                else{
+                    Console.WriteLine("Aun no se anoto nadie");
+                    Console.WriteLine("Presione una telca para continuar...");
+                    Console.ReadKey();
+                } 
+                menu();
+                break;
+            case 4:
+                if (habilitado) cambiarEntrada();
+                else{
+                    Console.WriteLine("Aun no se anoto nadie");
+                    Console.WriteLine("Presione una telca para continuar...");
+                    Console.ReadKey();
+                }
+                menu();
+                break;
+            case 5:
+                Console.Clear();
+                Console.WriteLine("Saliendo del programa...");
+                Environment.Exit(-1);
+                break;
+        }
+    }
+
+    void nuevaIncripcion(){
+        Dictionary<int, int> dicPrecios = new Dictionary<int, int>(){
+        { 1, 15000 },
+        { 2, 30000 },
+        { 3, 10000 },
+        { 4, 40000 }
+        };
+        Console.Clear();
+        Console.WriteLine("**********************************");
+        Console.WriteLine("Gracas por comenzar tu instripcion");
+        Console.WriteLine("**********************************");
+        Console.WriteLine("Eliga una opción para continuar");
+        Console.WriteLine("1. Dia 1 - $15.000");
+        Console.WriteLine("2. Dia 2 - $30.000");
+        Console.WriteLine("3. Dia 3 - $10.000");
+        Console.WriteLine("4. Full Pass - $40.000");
+        Console.WriteLine("5. Salir");
+        int opcion = int.Parse(Console.ReadLine());
+        while (opcion < 1 || opcion > 5) opcion = ingresarEntero("Esa opcion no existe, favor de ingresar una valida");
+        if (opcion == 5){
             Console.Clear();
             Console.WriteLine("Saliendo del programa...");
             Environment.Exit(-1);
-            break;
+        }
+        int totalABonado = dicPrecios[opcion];
+        Console.WriteLine("----------------------------------");
+        Console.WriteLine("Perfecto, ahora sus datos");
+        int dni = ingresarEntero("Ingrese su DNI");
+        string strDNI = dni.ToString();
+        while(strDNI.Length != 8){
+            dni = ingresarEntero("El DNI invalido | Un DNI valido tiene 8 digitos | Ingreselo nuevamente");
+            strDNI = dni.ToString();
+        }
+        string nombre = ingresarTexto("Ahora su nombre porfavor");
+        string apellido = ingresarTexto("Seguimos con su apellido");
+        Cliente cliente = new Cliente(dni, apellido, nombre, DateTime.Now, opcion, totalABonado);
+        miTicketera.agregarCliente(cliente);
     }
-}
 
-void nuevaIncripcion(){
-    int tipoEntrada, totalABonado = 0;
-    Console.Clear();
-    Console.WriteLine("**********************************");
-    Console.WriteLine("Gracas por comenzar tu instripcion");
-    Console.WriteLine("**********************************");
-    Console.WriteLine("Eliga una opción para continuar");
-    Console.WriteLine("1. Dia 1 - $15.000");
-    Console.WriteLine("2. Dia 2 - $30.000");
-    Console.WriteLine("3. Dia 3 - $10.000");
-    Console.WriteLine("4. Full Pass - $40.000");
-    Console.WriteLine("5. Salir");
-    int opcion = int.Parse(Console.ReadLine());
-    while (opcion < 1 || opcion > 5) opcion = ingresarEntero("Esa opcion no existe, favor de ingresar una valida");
-    switch (opcion){
-        case 1:
-            totalABonado = 15000;
-            break;
-        case 2:
-            totalABonado = 30000;
-            break;
-        case 3:
-            totalABonado = 10000;
-            break;
-        case 4:
-            totalABonado = 40000;
-            break;
-        case 5:
-            Console.Clear();
-            Console.WriteLine("Saliendo del programa...");
-            Environment.Exit(-1);
-            break;
-    }  
-    tipoEntrada = opcion;
-    Console.WriteLine("----------------------------------");
-    Console.WriteLine("Perfecto, ahora sus datos");
-    int dni = ingresarEntero("Ingrese su DNI");
-    string strDNI = dni.ToString();
-    while(strDNI.Length != 8){
-        dni = ingresarEntero("El DNI invalido | Un DNI valido tiene 8 digitos | Ingreselo nuevamente");
-        strDNI = dni.ToString();
+    void estadisticasEvento(){
+        List<string> listaPImprimir = miTicketera.EstadisticasTicketera();
+        for (int i = 0; i<listaPImprimir.Count(); i++){
+            Console.WriteLine(listaPImprimir[i]);
+        }
+        Console.WriteLine("Presione una tecla para continuar...");
+        Console.ReadKey();
     }
-    string nombre = ingresarTexto("Ahora su nombre porfavor");
-    string apellido = ingresarTexto("Seguimos con su apellido");
 
-    Cliente cliente = new Cliente(dni, apellido, nombre, DateTime.Now, tipoEntrada, totalABonado);
-}
+    void buscarCliente(){
 
-void estadisticasEvento(){
+    }
 
-}
-
-void buscarCliente(){
-
-}
-
-void cambiarEntrada(){
-    
-}
+    void cambiarEntrada(){
+        
+    }
 
 
-int ingresarEntero(string txt){
-    Console.WriteLine(txt);
-    return int.Parse(Console.ReadLine());;
-}
+    int ingresarEntero(string txt){
+        Console.WriteLine(txt);
+        return int.Parse(Console.ReadLine());;
+    }
 
-string ingresarTexto(string txt){
-    Console.WriteLine(txt);
-    return Console.ReadLine();
-}
+    string ingresarTexto(string txt){
+        Console.WriteLine(txt);
+        return Console.ReadLine();
+    }
--- Ticketera.cs
+++ Ticketera.cs
@@ -1,17 +1,19 @@
 using System;
-class Ticketera{
-    static private Dictionary<int, Cliente> dicClientes;
-    static private int ultimoIdEntrada = 1;
+class Ticketera {
+    private Dictionary<int, Cliente> dicClientes = new Dictionary<int, Cliente>();
+    private int ultimoIdEntrada = 1;
 
-    public int devolverUltimoID(){
+    public int devolverUltimoID() {
         return ultimoIdEntrada;
     }
-    public int agregarCliente(Cliente cliente){
+
+    public int agregarCliente(Cliente cliente) {
         int id = ultimoIdEntrada;
         dicClientes.Add(ultimoIdEntrada, cliente);
         ultimoIdEntrada++;
         return id;
     }
+
     Cliente buscarCliente(int idEntrada){
         if (dicClientes.ContainsKey(idEntrada)) return dicClientes[idEntrada];
         else return null;
@@ -26,12 +28,43 @@ class Ticketera{
         return pudo;
         
     }
-    List<string> EstadisticasTicketera(){
-        List<string> listaEstadisticas = null;
-
-
-
-        return listaEstadisticas;
-        
+    public List<string> EstadisticasTicketera(){
+        List<string> listaEstadisticas = new List<string>();
+        int tipo1, tipo2, tipo3, tipo4, rTipo1, rTipo2, rTipo3, rTipo4, total;
+        tipo1 = tipo2 = tipo3 = tipo4 = rTipo1 = rTipo2 = rTipo3 = rTipo4 = 0;
+        total = dicClientes.Count();
+        for (int i = 1; i<=dicClientes.Count(); i++){
+            switch(dicClientes[i].TipoEntrada){
+                case 1:
+                    tipo1++;
+                    rTipo1 += dicClientes[i].TotalAbonado;
+                    break;
+                case 2:
+                    tipo2++;
+                    rTipo2 += dicClientes[i].TotalAbonado;
+                    break;
+                case 3:
+                    tipo3++;
+                    rTipo3 += dicClientes[i].TotalAbonado;
+                    break;
+                case 4:
+                    tipo4++;
+                    rTipo4 += dicClientes[i].TotalAbonado;
+                    break;
+            }
+        }
+        listaEstadisticas.Add("Clientes inscriptos: " + dicClientes.Count());
+        listaEstadisticas.Add("Porcantaje de entradas");
+        listaEstadisticas.Add("   - Dia 1: " + tipo1 / total * 100 + "%");
+        listaEstadisticas.Add("   - Dia 2: " + tipo2 / total * 100 + "%");
+        listaEstadisticas.Add("   - Dia 3: " + tipo3 / total * 100 + "%");
+        listaEstadisticas.Add("   - Full Pass: " + tipo4 / total * 100 + "%");
+        listaEstadisticas.Add("Recaudacion de cada tipo");
+        listaEstadisticas.Add("   - Dia 1: $" + rTipo1);
+        listaEstadisticas.Add("   - Dia 2: $" + rTipo2);
+        listaEstadisticas.Add("   - Dia 3: $" + rTipo3);
+        listaEstadisticas.Add("   - Full Pass: $" + rTipo4);
+        listaEstadisticas.Add("Recaudacion Total: $" + (rTipo1 + rTipo2 + rTipo3 + rTipo4));
+        return listaEstadisticas; 
     }
 }
